9d831f45cffdd3a1322c0993c7f0f9c920ce917a
JamesSerrago <43243567+JamesSerrago@users.noreply.github.com>
2019-12-11 18:29:13 -0500

    Add files via upload

---
+++ My-Online-Library/inc/data.php
@@ -0,0 +1,165 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "1984",
+	"img" => "img/media/1984.jpg",
+    "genre" => "Science-Fiction",
+    "format" => "Paperback",
+    "year" => 1949,
+    "category" => "Books",
+    "authors" => [
+        "George Orwell"
+    ],
+    "publisher" => "Berkley",
+    "isbn" => '0452262933'
+];
+$catalog[102] = [
+    "title" => "The Shining",
+    "img" => "img/media/theshining.jpg",
+    "genre" => "Horror",
+    "format" => "Paperback",
+    "year" => 2013,
+    "category" => "Books",
+    "authors" => [
+        "Stephen King"
+    ],
+    "publisher" => "Anchor Reprint edition",
+    "isbn" => '0452262933'
+];
+$catalog[103] = [
+    "title" => "War of the Worlds",
+    "img" => "img/media/waroftheworlds.jpg",
+    "genre" => "Science-Fiction",
+    "format" => "Paperback",
+    "year" => 2010,
+    "category" => "Books",
+    "authors" => [
+        "H.G. Wells"
+    ],
+    "publisher" => "Tribeca Books",
+    "isbn" => '1936594056'
+];
+$catalog[104] = [
+    "title" => "The Time Machine",
+    "img" => "img/media/thetimemachine.jpg",
+    "genre" => "Science-Fiction",
+    "format" => "Paperback",
+    "year" => 2010,
+    "category" => "Books",
+    "authors" => [
+        "H.G. Wells"
+    ],
+    "publisher" => "Tribeca Books",
+    "isbn" => '1936594110'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Back to the Future",
+    "img" => "img/media/backtothefuture.jpg",
+    "genre" => "Science-Fiction",
+    "format" => "Blu-Ray",
+    "year" => 1985,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Robert Zemeckis",
+        "Bob Gale"
+    ],
+    "stars" => [
+        "Michael J. Fox",
+        "Christopher Lloyd",
+    ]
+];
+$catalog[202] = [
+    "title" => "Saving Private Ryan",
+    "img" => "img/media/savingprivateryan.jpg",
+    "genre" => "Action, Adventure, Drama",
+    "format" => "Blu-Ray",
+    "year" => 1998,
+    "category" => "Movies",
+    "director" => "Steven Spielberg",
+    "writers" => [
+        "Robert Rodat",
+    ],
+    "stars" => [
+			"Tom Hanks",
+			"Edward Burns",
+			"Barry Pepper"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Matrix",
+    "img" => "img/media/thematrix.jpg",
+    "genre" => "Action",
+    "format" => "Blu-Ray",
+    "year" => 1999,
+    "category" => "Movies",
+    "director" => "Andy Wachowski",
+    "writers" => [
+        "Lana Wachowski",
+        "Lilly Wachowski"
+    ],
+    "stars" => [
+			"Keanu Reeves",
+			"Carrie-Anne Moss",
+			"Lawrence Fishburne"
+    ]
+];
+$catalog[204] = [
+    "title" => "The Thing",
+		"img" => "img/media/thething.jpg",
+	  "genre" => "Action, Adventure, Fantasy",
+	  "format" => "Blu-Ray",
+	  "year" => 1982,
+	  "category" => "Movies",
+	  "director" => "John Carpenter",
+	  "writers" => [
+	      "Bill Lancaster"
+
+	  ],
+	  "stars" => [
+	      "Kurt Russell",
+	      "Wilford Brimley",
+	      "T.K. Carter"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "The Dark Side Of The Moon",
+	"img" => "img/media/darksideofthemoon.jpg",
+	"genre" => "Progressive rock",
+	"format" => "CD",
+	"year" => 2012,
+	"category" => "Music",
+	"artist" => "Pink Floyd"
+];
+
+$catalog[302] = [
+	"title" => "American IV: The Man Comes Around",
+	"img" => "img/media/themancomesaround.jpg",
+	"genre" => "Acoustic rock",
+	"format" => "CD",
+	"year" => 2002,
+	"category" => "Music",
+	"artist" => "Johnny Cash"
+];
+$catalog[303] = [
+	"title" => "Johnny Cash",
+	"img" => "img/media/black.jpg",
+	"genre" => "Heavy Metal",
+	"format" => "CD",
+	"year" => 1991,
+	"category" => "Music",
+	"artist" => "Metallica"
+];
+$catalog[304] = [
+	"title" => "Ready To Die",
+	"img" => "img/media/readytodie.jpg",
+	"genre" => "East Coast hip hop",
+	"format" => "CD",
+	"year" => 1994,
+	"category" => "Music",
+	"artist" => "The Notorious B.I.G"
+];
+?>
